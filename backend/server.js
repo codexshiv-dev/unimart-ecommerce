943f154db113aff3eb272b449450cf0e05e1f5aa
Gupta shiv <codexshiv.dev@gmail.com>
2026-03-19 23:28:55 +0530

    update: improve folder structure

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Unimart Backend Running...");
+});
+
+// Routes
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
