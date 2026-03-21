ece3be3c60c161639fe12ca1db65aa20d2c654ed
Gupta shiv <codexshiv.dev@gmail.com>
2026-03-21 22:29:13 +0530

    Removed backend folder

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Test route
-router.get("/", (req, res) => {
-  res.json({ message: "Products route working" });
-});
-
-module.exports = router;
